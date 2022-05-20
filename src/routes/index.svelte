50f136fc5ebe01592eceaf9227f8e7b67bd11426
Panumat inud <banzwingzaa@hotmail.com>
2022-05-20 20:47:14 +0700

    TEST netify 1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
+<script>
+	import Buttom from '@smui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Buttom variant="outlined" color="primary">OK</Buttom>
+
